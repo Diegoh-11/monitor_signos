a7c924ec321d71eb3215218811221eefdcd3411b
diegoh-11 <dherreraal12@cesde.net>
2026-03-08 18:39:48 -0500

    Se creo la funcion 1 pedir y validar el BPM

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,20 @@
 // ── Variables globales ──────────────────────────
-
 let criticos = 0;
 let alertasLeves = 0;
 let totalBpm = 0;
 let lecturas = 0;
 let misionActiva = true;
+
+// ── Función 1: Pedir y validar el BPM ──────────
+function pedirBpm() {
+    let entrada = prompt("Ingresa el ritmo cardiaco del astronauta (BPM).\nNúmero negativo para terminar misión:");
+    let bpm = parseFloat(entrada);
+
+    if (isNaN(bpm)) {
+        alert("Dato inválido. Debes ingresar un número.");
+        return pedirBpm();
+    }
+
+    return bpm;
+}
+
